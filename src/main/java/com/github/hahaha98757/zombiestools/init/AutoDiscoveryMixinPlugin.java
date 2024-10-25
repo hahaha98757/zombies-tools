9ad69f7a7018011ba953f17ced54fb54cc88efa4
GitHub Action <action@github.com>
2024-10-25 09:03:25 +0000

    Initialize template repository proper
    Set package name to com.github.hahaha98757.zombiestools
    Set project name to zombies-tools
    Set modid to zombiestools

--- src/main/java/com/github/hahaha98757/zombiestools/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/hahaha98757/zombiestools/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.hahaha98757.zombiestools.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
